636ba9af5b9d61094fcbcb044401883b215eddae
panchelo <lorenzo.panchetti@schindler.com>
2022-01-25 09:58:51 +0100

    typing annotations, imports section and variables naming fixed

--- RWS_wrapper.py
+++ RWS_wrapper.py
@@ -1,12 +1,15 @@
+from typing import Union
+
 import RWS2
-import string
 
 
 class RwsWrapper:
-    def __init__(self, robot_url):
+    def __init__(self, robot_url: str):
         self.robot = RWS2.RWS(robot_url)
 
-    def set_RAPID_variable(self, variable_name: string, new_value):
+    def set_RAPID_variable(
+        self, variable_name: str, new_value: Union[float, int, bool, str]
+    ):
         self.robot.request_mastership()
         self.robot.set_rapid_variable(variable_name, new_value)
         self.robot.release_mastership()
@@ -16,7 +19,7 @@ class RwsWrapper:
         self.robot.motors_on()
         self.robot.release_mastership()
 
-    def complete_instruction(self, var="ready_flag"):
+    def complete_instruction(self, var: str = "ready_flag"):
         self.turn_motors_on()
         self.robot.start_RAPID()
         self.robot.wait_for_rapid()
