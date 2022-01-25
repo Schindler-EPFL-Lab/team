88b616e8fdf2d0c988409967a2f6b17789a8b916
panchelo <lorenzo.panchetti@schindler.com>
2022-01-25 10:54:49 +0100

    return type and reset_pp parameter added

--- RWS_wrapper.py
+++ RWS_wrapper.py
@@ -9,19 +9,21 @@ class RwsWrapper:
 
     def set_RAPID_variable(
         self, variable_name: str, new_value: Union[float, int, bool, str]
-    ):
+    ) -> None:
         self.robot.request_mastership()
         self.robot.set_rapid_variable(variable_name, new_value)
         self.robot.release_mastership()
 
-    def turn_motors_on(self):
+    def turn_motors_on(self) -> None:
         self.robot.request_mastership()
         self.robot.motors_on()
         self.robot.release_mastership()
 
-    def complete_instruction(self, var: str = "ready_flag"):
+    def complete_instruction(
+        self, reset_pp: bool = False, var: str = "ready_flag"
+    ) -> None:
         self.turn_motors_on()
-        self.robot.start_RAPID()
+        self.robot.start_RAPID(reset_pp)
         self.robot.wait_for_rapid()
         self.robot.stop_RAPID()
         self.set_RAPID_variable(var, "FALSE")
