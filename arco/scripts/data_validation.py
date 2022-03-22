d99fdb2fe5529d91aef4a20b9fe6458463ad691a
panchelo <lorenzo.panchetti@schindler.com>
2022-03-22 11:58:32 +0100

    Added main to file

--- arco/scripts/data_validation.py
+++ arco/scripts/data_validation.py
@@ -2,7 +2,8 @@ from arco.utility.handling_data import get_demo_files, check_data_timestamps, \
     check_nan_values, check_reading_files
 
 
-list_files = get_demo_files()
-check_reading_files(list_files)
-check_nan_values(list_files)
-check_data_timestamps(list_files)
+if __name__ == "__main__":
+    list_files = get_demo_files()
+    check_reading_files(list_files)
+    check_nan_values(list_files)
+    check_data_timestamps(list_files)
