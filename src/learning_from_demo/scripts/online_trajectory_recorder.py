fc41755d9b19e5df4eadcef83bb0d374f5c86837
panchelo <lorenzo.panchetti@schindler.com>
2022-05-15 16:37:51 +0200

    Added main

--- src/learning_from_demo/scripts/online_trajectory_recorder.py
+++ src/learning_from_demo/scripts/online_trajectory_recorder.py
@@ -14,11 +14,13 @@ parser = argparse.ArgumentParser(
 )
 
 # Add argument which takes the url to be used for the communication
-parser.add_argument("--url", type=str, required=True, help="Url address to send "
-                    "requests to")
+parser.add_argument(
+    "--url", type=str, required=True, help="Url address to send " "requests to"
+)
 # Add argument which takes the file destination path to be used for data storing
-parser.add_argument("--dest_path", type=str, required=True, help="Destination path to"
-                    "save data")
+parser.add_argument(
+    "--dest_path", type=str, required=True, help="Destination path to" "save data"
+)
 # Parse the command line arguments to an object
 args = parser.parse_args()
 
@@ -34,27 +36,34 @@ if os.path.splitext(args.dest_path)[1] != ".json":
     print("Only .json files are accepted")
     exit()
 
-try:
-    record = DemonstrationRecorder(path_to_store_demo=args.dest_path)
-    rws_robot = RWS(args.url)
-    data = create_default_dict()
-    # wait for the RAPID program to start
-    while not rws_robot.is_running():
-        pass
-    # since RWS2 doesn't return a timestamp with the measurement, compute it from Python
-    t_start = timer()
-    # record until RAPID program stops
-    while rws_robot.is_running():
-        tcp_pos, tcp_ori, rob_cf = rws_robot.get_tcp_info()
-        joints = rws_robot.get_joints_positions()
-        timestamp = [timer() - t_start]
-        values_list = timestamp + tcp_pos + tcp_ori + rob_cf + joints
-        # check that all information are available
-        if tcp_pos and tcp_ori and rob_cf and joints:
-            info = {key: value for (key, value) in zip(data.keys(), values_list)}
-            record.update(tmp_dict=info)
-    # save data to file
-    record.create_file()
 
-finally:
-    pass
+def main():
+    try:
+        record = DemonstrationRecorder(path_to_store_demo=args.dest_path)
+        rws_robot = RWS(args.url)
+        data = create_default_dict()
+        # wait for the RAPID program to start
+        while not rws_robot.is_running():
+            pass
+        # since RWS2 doesn't return a timestamp with the measurement
+        # compute it from Python
+        t_start = timer()
+        # record until RAPID program stops
+        while rws_robot.is_running():
+            tcp_pos, tcp_ori, rob_cf = rws_robot.get_tcp_info()
+            joints = rws_robot.get_joints_positions()
+            timestamp = [timer() - t_start]
+            values_list = timestamp + tcp_pos + tcp_ori + rob_cf + joints
+            # check that all information are available
+            if tcp_pos and tcp_ori and rob_cf and joints:
+                info = {key: value for (key, value) in zip(data.keys(), values_list)}
+                record.update(tmp_dict=info)
+        # save data to file
+        record.create_file()
+
+    finally:
+        pass
+
+
+if __name__ == "__main__":
+    main()
