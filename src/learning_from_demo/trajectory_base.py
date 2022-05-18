bb45616e4d600a5c37ddd970bc3bcff36737f1b0
panchelo <lorenzo.panchetti@schindler.com>
2022-05-18 10:15:09 +0200

    Added method to flip trajectories

--- src/learning_from_demo/trajectory_base.py
+++ src/learning_from_demo/trajectory_base.py
@@ -69,6 +69,14 @@ class TrajectoryBase(ABC):
         selected_waypoint.append(self._joint_to_string(self.get_joints_at_index(-1)))
         return "\n".join(selected_waypoint)
 
+    def flip_trajectory(self) -> np.ndarray:
+        """
+        Flips the trajectory array along the time axis
+
+        :return: flipped trajectory array
+        """
+        return np.flip(self._trajectory, axis=0)
+
     @staticmethod
     def _joint_to_string(joints: np.ndarray) -> str:
         """
