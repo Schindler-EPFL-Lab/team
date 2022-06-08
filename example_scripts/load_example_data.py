4538563d6d281f55927997d6cf325e71f3efb3cc
panchelo <lorenzo.panchetti@schindler.com>
2022-06-08 13:41:54 +0200

    Added data loader

---
+++ example_scripts/load_example_data.py
@@ -0,0 +1,40 @@
+import subprocess
+
+
+def main():
+    subprocess.run(
+        [
+            "dvc",
+            "get",
+            "git@ssh.dev.azure.com:v3/devsdb/CRD-NT_ARCO/arco-datasets",
+            "demonstrations/door_opening/door_opening_lp_1.json",
+            "-o",
+            "demonstrations/door_opening/door_opening_lp_1.json",
+        ]
+    )
+
+    subprocess.run(
+        [
+            "dvc",
+            "get",
+            "git@ssh.dev.azure.com:v3/devsdb/CRD-NT_ARCO/arco-datasets",
+            "demonstrations/door_opening/door_opening_lp_2.json",
+            "-o",
+            "demonstrations/door_opening/door_opening_lp_2.json",
+        ]
+    )
+
+    subprocess.run(
+        [
+            "dvc",
+            "get",
+            "git@ssh.dev.azure.com:v3/devsdb/CRD-NT_ARCO/arco-datasets",
+            "demonstrations/door_opening/door_opening_lp_3.json",
+            "-o",
+            "demonstrations/door_opening/door_opening_lp_3.json",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
