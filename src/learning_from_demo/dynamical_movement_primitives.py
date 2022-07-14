Fixed wrong method name and added test to check correct rms computation

src/learning_from_demo/dynamical_movement_primitives.py
@@ -253,7 +253,7 @@ class DynamicMovementPrimitives:
         """
         self.set_alpha_z_and_n_rfs(alpha_z, n_rbfs)
         dmp_trajectory = self.compute_joint_dynamics(goal=self._G, y_init=self._y0)
-        mse_error = dmp_trajectory.mse_error(self._regression_trajectory)
+        mse_error = dmp_trajectory.rms_error(self._regression_trajectory)
         final_error = np.linalg.norm(dmp_trajectory.joints[-1] - self._G)
         return mse_error + final_error
 

src/learning_from_demo/trajectory_base.py
@@ -30,7 +30,7 @@ class TrajectoryBase(ABC):
     def __len__(self) -> int:
         return len(self._trajectory)
 
-    def mse_error(self, other_trajectory: "TrajectoryBase") -> float:
+    def rms_error(self, other_trajectory: "TrajectoryBase") -> float:
         """
         Compute the mean squared error along the motion between the trajectory to track
         and the executed trajectory
@@ -45,7 +45,7 @@ class TrajectoryBase(ABC):
             error += np.linalg.norm(
                 self.get_joints_at_index(i) - other_trajectory.get_joints_at_index(i)
             )
-        return error / len(self.joints)
+        return error / np.sqrt(len(self.joints))
 
     def joints_to_string(self, tol_diff: int = 1) -> str:
         """