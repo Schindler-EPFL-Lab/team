dd32ba4f4f736dc26a0412705d8f0119579ed7d2
LorenzoPanchetti <lorenzo.panchetti@epfl.ch>
2023-05-14 19:10:36 +0200

    Improved code

--- scripts/results_generation/results_gmcc_metric.py
+++ scripts/results_generation/results_gmcc_metric.py
@@ -1,21 +1,42 @@
 import os
 
+import json
 import numpy as np
 import pandas as pd
 
 from team.utility.accuracy_metric import symmetric_gmcc
 
 
-def extract_tcp_into_numpy(data_path: str) -> np.ndarray:
+def extract_data_into_numpy(data_path: str, keyword: str) -> np.ndarray:
     """
-    Extract tcp data from json dictionary into numpy array
+    Extract data from json dictionary into numpy array
 
     :param data_path: data path
+    :param keyword: string to select tcp or joint data
     :return: extracted data in numpy array
     """
+    if keyword == "tcp":
+        list_keys = ["tcp_x", "tcp_y", "tcp_z"]
+    elif keyword == "joint":
+        list_keys = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"]
+    else:
+        raise ValueError("Keywork argument is not valid, insert 'tcp' or 'joint'")
     df = pd.read_json(data_path)
-    tcp = df[["tcp_x", "tcp_y", "tcp_z"]]
-    return tcp.to_numpy()
+    data = df[list_keys]
+    return data.to_numpy()
+
+
+def get_trajectory_data(data_path: str) -> np.ndarray:
+    """
+    Extract trajectory data from json dictionary
+
+    :param data_path: data path
+    :return: trajectory
+    """
+    with open(data_path, 'r') as f:
+        data = json.load(f)
+    traj = data["trajectory"]
+    return np.array(traj)
 
 
 def first_is_longer(traj_1: np.ndarray, traj_2: np.ndarray) -> bool:
@@ -37,6 +58,7 @@ def get_motion_onset_idx(trajectory: np.ndarray, tol: float) -> int:
     :param tol: tolerance to assess motion onset
     :return: index at which motion starts
     """
+    nb_samples = np.shape(trajectory)[0]
     dims = np.shape(trajectory)[1]
     list_idx = []
     # Loop on each dimension
@@ -44,6 +66,8 @@ def get_motion_onset_idx(trajectory: np.ndarray, tol: float) -> int:
         j = 0
         while (abs(trajectory[j, i] - trajectory[j+1, i]) < tol):
             j = j+1
+            if j == nb_samples - 1:
+                break
         list_idx.append(j)
     # Take lowest index as some dimensions might not be affected by motion
     return min(list_idx)
@@ -58,7 +82,8 @@ def pad_end(trajectory: np.ndarray, final_len: int) -> np.ndarray:
     :return: padded trajectory
     """
     nb_samples = final_len - np.shape(trajectory)[0]
-    extension = np.full_like(trajectory, trajectory[-1, :], shape=(nb_samples, 3))
+    nb_dims = np.shape(trajectory)[1]
+    extension = np.full_like(trajectory, trajectory[-1, :], shape=(nb_samples, nb_dims))
     return np.row_stack([trajectory, extension])
 
 
@@ -87,20 +112,22 @@ def comparison_setup(regression: np.ndarray, reproduction: np.ndarray) -> list[
     return [trimmed_regression, trimmed_reproduction]
 
 
-def get_gmcc_score(regression_path: str, reproduction_path: str) -> float:
+def get_gmcc_score(regression_path: str, reproduction_path: str,
+                   data_type: str) -> float:
     """_summary_
 
     :param regression_path: regression data path
     :param reproduction_path: reproduction data path
+    :param data_type: tcp or joint data
     :return: gmcc score
     """
     file_extension = os.path.splitext(regression_path)[1]
     if file_extension == ".json":
-        regression = extract_tcp_into_numpy(regression_path)
+        regression = extract_data_into_numpy(regression_path, data_type)
     else:
         # Account for regression files in .npy
         regression = np.load(regression_path)
-    reproduction = extract_tcp_into_numpy(reproduction_path)
+    reproduction = extract_data_into_numpy(reproduction_path, data_type)
     (regre, repro) = comparison_setup(regression, reproduction)
     return symmetric_gmcc(regre, repro)
 
@@ -114,71 +141,71 @@ if __name__ == "__main__":
     # Brush homing score
     regression_path = os.path.join(data_dir, "brush_homing/tcp_regression.npy")
     reproduction_path = os.path.join(data_dir, "brush_homing/brush_homing_repro.json")
-    gmcc_value_bh = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_bh = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Brush picking up score
     regression_path = os.path.join(data_dir, "brush_picking_up/tcp_regression.npy")
     reproduction_path = os.path.join(data_dir,
                                      "brush_picking_up/brush_picking_up_repro.json")
-    gmcc_value_pu = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_pu = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Door closing score
     regression_path = os.path.join(data_dir, "door_closing/tcp_regression.npy")
     reproduction_path = os.path.join(data_dir,
                                      "door_closing/door_closing_repro.json")
-    gmcc_value_dc = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_dc = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Door opening score
     regression_path = os.path.join(data_dir, "door_opening/tcp_regression.npy")
     reproduction_path = os.path.join(data_dir,
                                      "door_opening/door_opening_repro.json")
-    gmcc_value_do = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_do = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Rail cleaning score
     regression_path = os.path.join(data_dir, "rail_cleaning/tcp_regression.npy")
     reproduction_path = os.path.join(data_dir,
                                      "rail_cleaning/rail_cleaning_repro.json")
-    gmcc_value_rc = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_rc = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Drill task score
     regression_path = os.path.join(data_dir, "drill/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir, "drill/drill_repro/demonstration_1.json")
-    gmcc_value_drill = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_drill = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Drill far task score
     regression_path = os.path.join(data_dir, "drill/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir, "drill/drill_far/demonstration_1.json")
-    gmcc_value_dr_f = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_dr_f = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Timo avoid task score
     regression_path = os.path.join(data_dir,
                                    "Timo_avoid/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir,
                                      "Timo_avoid/repro/demonstration_1.json")
-    gmcc_value_ta = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_ta = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Timo avoid far task score
     regression_path = os.path.join(data_dir,
                                    "Timo_avoid/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir,
                                      "Timo_avoid/repro_far/demonstration_1.json")
-    gmcc_value_ta_f = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_ta_f = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Timo collab task score
     regression_path = os.path.join(data_dir,
                                    "Timo_collab/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir,
                                      "Timo_collab/repro/demonstration_1.json")
-    gmcc_value_tc = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_tc = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Timo collab far task score
     regression_path = os.path.join(data_dir,
                                    "Timo_collab/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir,
                                      "Timo_collab/repro_far/demonstration_1.json")
-    gmcc_value_tc_f = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_tc_f = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Timo difficult task score
     regression_path = os.path.join(data_dir,
                                    "Timo_difficult/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir,
                                      "Timo_difficult/repro/demonstration_1.json")
-    gmcc_value_td = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_td = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Timo difficult far task score
     regression_path = os.path.join(data_dir,
                                    "Timo_difficult/regression/demonstration_1.json")
     reproduction_path = os.path.join(data_dir,
                                      "Timo_difficult/repro_far/demonstration_1.json")
-    gmcc_value_td_f = get_gmcc_score(regression_path, reproduction_path)
+    gmcc_value_td_f = get_gmcc_score(regression_path, reproduction_path, "tcp")
     # Drill data
     drill = [gmcc_value_drill, gmcc_value_dr_f]
     # Avoid data
